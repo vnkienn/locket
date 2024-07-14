e7c9ff8565bf19896de46fccb95dd5dcb72b3920
vnkienn <149071222+vnkienn@users.noreply.github.com>
2024-07-14 10:42:30 +0700

    Update Locket_Gold.js

--- Locket_Gold.js
+++ Locket_Gold.js
@@ -15,17 +15,17 @@ const ent = JSON.parse(data);
 
 let jsonToUpdate = {
         "request_date_ms": 1704070861000,
-        "request_date": "2006-04-09T01:01:01Z",
+        "request_date": "2007-11-18T01:01:01Z",
         "subscriber": {
             "entitlement": {},
-            "first_seen": "2006-04-09T01:01:01Z",
+            "first_seen": "2007-11-18T01:01:01Z",
             "original_application_version": "9692",
-            "last_seen": "2006-04-03T01:01:01Z",
+            "last_seen": "2007-11-18T01:01:01Z",
             "other_purchases": {},
             "management_url": null,
             "subscriptions": {},
             "entitlements": {},
-            "original_purchase_date": "2006-04-09T01:01:01Z",
+            "original_purchase_date": "2007-11-18T01:01:01Z",
             "original_app_user_id": "70B24288-83C4-4035-B001-573285B21AE2",
             "non_subscriptions": {}
         }
@@ -40,7 +40,7 @@ for (const [entitlementId, productInfo] of Object.entries(productEntitlementMapp
 
   for (const entitlement of entitlements) {
     jsonToUpdate.subscriber.entitlements[entitlement] = {
-      "purchase_date": "2006-04-09T01:01:01Z",
+      "purchase_date": "2007-11-18T01:01:01Z",
       "original_purchase_date": "2006-04-03T01:01:01Z",
       "expires_date": "9692-01-01T01:01:01Z",
       "is_sandbox" : false,
@@ -52,8 +52,8 @@ for (const [entitlementId, productInfo] of Object.entries(productEntitlementMapp
     // Add product identifier to subscriptions
     jsonToUpdate.subscriber.subscriptions[productIdentifier] = {
       "expires_date": "9692-01-01T01:01:01Z",
-      "original_purchase_date": "2006-04-09T01:01:01Z",
-      "purchase_date": "2006-04-09T01:01:01Z",
+      "original_purchase_date": "2007-11-18T01:01:01Z",
+      "purchase_date": "2007-11-18T01:01:01Z",
       "is_sandbox" : false,
       "ownership_type": "PURCHASED",
       "store": "app_store"
